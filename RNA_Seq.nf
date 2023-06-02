compbiomed/RNA_Seq

commit f4727873a7471c7c2a223ec8e2d67ea1d367967c

Update runSTARgenomeGenerate to accommodate STAR 2.79a
(Note: the first attempt at this commit mistakenly added lines to the end of process runSTAR1pass instead of runSTARgenomeGenerate; that attempt was reverted, and has been superseded by this one. - ACG)

The previous commit upgraded the default version of STAR from 2.6.0c to 2.7.9a, and the newer version creates the file Log.out in the path specified by the --genomeDir argument, rather than in the current working directory. As a result, the dedicated output channel for Log.out has been deprecated. An additional command has also been added at the end of the process to move Log.out into the genome directory if it exists (i.e., if the user chose to use an earlier version of STAR for some reason).

RNA_Seq.nf
@@ -191,7 +191,6 @@ process runSTARgenomeGenerate {
   file(ensembl_gtf_file) from generateGTF_to_runSTARgenomeGenerate
 
   output:
-  file("Log.out") into runSTARgenomeGenerateOutput
   file("${genomeDir}") into runSTARgenomeGenerate_to_runSTAR2pass
 
   script:
@@ -215,6 +214,10 @@ process runSTARgenomeGenerate {
     --sjdbOverhang ${params.read_length - 1} \
     --runThreadN \$NSLOTS \
     --limitSjdbInsertNsj ${params.STAR.limitSjdbInsertNsj}
+
+  # If an earlier version of STAR is used, move the file Log.out
+  # to the output directory (which is the behavior of later versions of STAR)
+  [[ -e Log.out ]] && mv -v Log.out ${params.STAR.genomeDir}/
   """
 }
 

commit 944446b55fb12f9941aef7877744683c2a883382

Fix another bug in runSTARgenomeGenerate
This commit fixes a trivial but frustrating bug introduced in runSTARgenomeGenerate in commit f4727873a7471c7c2a223ec8e2d67ea1d367967c.

At the end of the process, a conditional statement checks whether the file 'Log.out' is present in the current working directory, and if so, moves it to the output directory along with the other files.  However, the statement did not include any action to be performed if the file was not present, and so, in this case, it would return exit code 1, causing the process to fail (even if the genomeGenerate step worked correctly).  This case is now handled with the 'true' command to return exit code 0 if the file is not present.

RNA_Seq.nf
@@ -217,7 +217,7 @@ process runSTARgenomeGenerate {
 
   # If an earlier version of STAR is used, move the file Log.out
   # to the output directory (which is the behavior of later versions of STAR)
-  [[ -e Log.out ]] && mv -v Log.out ${genomeDir}/
+  [[ -e Log.out ]] && mv -v Log.out ${genomeDir}/ || true
   """
 }
 